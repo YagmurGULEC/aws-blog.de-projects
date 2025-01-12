8d72c0d36ae32a6ce920324e7f88c495d4b0ea76
YagmurGULEC <yagmurgulec89@gmail.com>
2025-01-12 08:03:47 -0500

    Add .gitignore to exclude other folders

--- sam-dash/frontend/dash_app.py
+++ sam-dash/frontend/dash_app.py
@@ -20,13 +20,8 @@ def build_app(dash_kwargs: dict = None) -> Dash:
             html.Div(
                 [
                     html.H2("How many days are you old?"),
-                    html.P("Select your Birthdate: "),
-                    dcc.DatePickerSingle(
-                        id="birthdate-picker",
-                        date=date(1993, 1, 1),
-                        display_format="DD.MM.YYYY",
-                    ),
-                    html.P(id="birthdate-output"),
+                   
+                   
                 ]
             ),
         ],
@@ -39,19 +34,19 @@ def build_app(dash_kwargs: dict = None) -> Dash:
     return app
 
 
-@callback(
-    Output("birthdate-output", "children"),
-    Input("birthdate-picker", "date"),
-    prevent_initial_call=True,
-)
-def calculate_days_since_birth(birthday_iso_string):
+# @callback(
+#     Output("birthdate-output", "children"),
+#     Input("birthdate-picker", "date"),
+#     prevent_initial_call=True,
+# )
+# def calculate_days_since_birth(birthday_iso_string):
 
-    now = datetime.now().date()
-    birthday = date.fromisoformat(birthday_iso_string)
+#     now = datetime.now().date()
+#     birthday = date.fromisoformat(birthday_iso_string)
 
-    age_in_days = (now - birthday).days
-    output = ["You're ", html.Strong(age_in_days), " days young!"]
-    return output
+#     age_in_days = (now - birthday).days
+#     output = ["You're ", html.Strong(age_in_days), " days young!"]
+#     return output
 
 
 if __name__ == "__main__":
